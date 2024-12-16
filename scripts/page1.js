1010ed76754e4c49afc1eca6d04ffbe50cbf2015
Harshit Tiwari <harshit54646587@gmail.com>
2024-12-16 13:52:11 +0530

    replaced typewriter animation with a new animation on #page1

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,28 +1,61 @@
-var msgs = [
-  "I am a passionate and dedicated software engineer with a keen eye for detail. My creativity shines through in my work as I create visually appealing and user-friendly interfaces. I am always eager to learn and improve my skills, so feel free to reach out if you're interested in collaborating on a project or simply discussing your interests.",
-  "I have a strong background in information technology and a strong interest in the field of web development and cybersecurity. I am currently looking for a job where I can learn new technologies and make new connections.",
-  "In my free time, I enjoy playing soccer, cooking, reading, and exploring the city. I also love to travel and visit new places.",
-  "I am currently available for freelance work or consulting. If you're interested in working together, please don't hesitate to reach out.",
-];
+function bioAnimation() {
+  const roles = document.querySelectorAll("#role h1");
+  const descriptions = document.querySelectorAll("#description h1");
+  const index = roles.length;
 
-function addSpan() {
-  var string = "";
-  msgs.forEach((msg, index) => {
-    msg.split("").map((letter) => {
-      string += `<span>${letter}</span>`;
+  gsap.set(roles, { y: "100%", opacity: 0 });
+
+  let currentIndex = 0;
+  animateText();
+  function animateText() {
+    const currentRole = roles[currentIndex];
+    const nextIndex = (currentIndex + 1) % index;
+    const nextRole = roles[nextIndex];
+
+    gsap.to(currentRole, {
+      y: -10,
+      opacity: 0,
+      duration: 1.1,
+      ease: "power4.inOut",
     });
-    document.getElementById("msg").innerHTML += `<p class="msgs">${string}</p>`;
-    string = "";
-  });
-}
+    gsap.fromTo(
+      nextRole,
+      { y: 20, opacity: 0 },
+      {
+        y: 5,
+        opacity: 1,
+        duration: 1,
+        ease: "power4.inOut",
+        delay: 0.4,
+      }
+    );
+    const currentDescription = descriptions[currentIndex];
+    const nextDescription = descriptions[nextIndex];
 
-function typewriter() {
-  gsap.from("#msg p span", {
-    opacity: 0,
-    stagger: 0.05,
-    duration: 0.01,
-  });
+    gsap.to(currentDescription, {
+      y: -10,
+      opacity: 0,
+      duration: 1.1,
+      ease: "power4.inOut",
+    });
+    gsap.fromTo(
+      nextDescription,
+      { y: 20, opacity: 0 },
+      {
+        y: 5,
+        opacity: 1,
+        duration: 1,
+        ease: "power4.inOut",
+        delay: 0.4,
+        onComplete: () => {
+          currentIndex = nextIndex;
+          animateText();
+        },
+      }
+    );
+  }
 }
+bioAnimation();
 
 function mainProfileAnimation() {
   gsap.to("#profileMain", {
@@ -34,13 +67,10 @@ function mainProfileAnimation() {
       scroller: "body",
       trigger: "#page1",
       start: "top -6%",
-      end: "top -10%",
+      end: "top -16%",
       pin: true,
       scrub: 0.5,
     }
   });
 }
-
-typewriter();
-addSpan();
 mainProfileAnimation();
